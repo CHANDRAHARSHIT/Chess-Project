5f73fa8af9ca9e32ed0f03616ae1576d88994fa1
Jayendra Singh Nayal <jayendranayal@gmail.com>
2026-07-27 11:49:51 +0530

    Removed: Unnecessory files and implementd global click

--- frontend/test-type.ts
+++
@@ -1,3 +0,0 @@
-import { ChessboardOptions } from "react-chessboard";
-const opt: ChessboardOptions = {};
-opt.onPieceDrop = (a, b, c) => true;
--- frontend/test-type3.ts
+++
@@ -1,5 +0,0 @@
-import { ChessboardOptions } from "react-chessboard";
-const opt: ChessboardOptions = {
-  onSquareClick: (sq) => {},
-  onPieceClick: (piece, sq) => {}
-};
