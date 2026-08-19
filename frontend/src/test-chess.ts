8e8676b7fb247c58062d5cae01791eb5837c1535
deepdas2607 <deepdas2607@gmail.com>
2026-08-19 19:05:27 +0530

    fix - remove unwanted file

--- frontend/src/test-chess.ts
+++
@@ -1,8 +0,0 @@
-import { Chess } from 'chess.js';
-const game = new Chess();
-try {
-  game.move({ from: 'e2', to: 'e4', promotion: 'q' });
-  console.log('success');
-} catch(e) {
-  console.log('error', e.message);
-}
