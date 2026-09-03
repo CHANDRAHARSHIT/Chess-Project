393d03db01e7923fc2105b28b31cfe245fdafa21
Harsh-Mahatha <harshmahatha1847@gmail.com>
2026-09-03 16:05:51 +0530

    feat: complete industry-standard architecture with barrel exports and atomic hierarchy

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,26 @@
+export * from "./billing-payment.types";
+export * from "./chess-chess.types";
+export * from "./joinus-assessmentTypes";
+export * from "./lessons-types";
+export * from "./openings-openings.types";
+export type {
+  ParticipantAssignment,
+  TimeControl,
+  MatchProvenance,
+  MatchDescriptor,
+  TicketStatus,
+  MatchTicket,
+  SessionStatus,
+  TerminationReason,
+  ResultOutcome,
+  ResultParticipant,
+  GameResult as MultiplayerGameResult,
+  Chess960State,
+  SessionClockSnapshot,
+  StateUpdatePayload,
+  PresenceUpdatePayload,
+  OutboundMessage,
+  InboundMessage,
+} from "./play-multiplayer.types";
+export * from "./puzzles-pathway.types";
+export * from "./puzzles-puzzle.types";
